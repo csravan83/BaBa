100345e8a0dc25b2d80ea6e0c57f4a5dbef048a9
Sravan Chennuri <csravan83@gmail.com>
2017-06-11 10:57:26 +0700

    added loading

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import {LoadingController} from 'ionic-angular'
 
 @Component({
   selector: 'page-login',
@@ -8,14 +9,24 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class Login {
 
-  constructor(public navCtrl: NavController, 
+  constructor(
+              public loadingCtrl: LoadingController,
+              public navCtrl: NavController,
               public navParams: NavParams,
               public afAuth: AngularFireAuth) {
   }
 
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Loading",
+      duration: 3000
+    });
+    loader.present();
+  }
+
   start(){
     this.afAuth.auth.signInAnonymously().then((info) => {
-      
+
     }).catch((error)=> {
       console.log(error)
     })
